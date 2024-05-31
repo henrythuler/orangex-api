ccdbbb5b22833c2cc20c968eb7776ee9aceb09b9
Henry Thuler <henrythuler2003@outlook.com>
2024-05-31 10:17:00 -0300

    feat(user): User repository and model created

---
+++ src/main/java/br/com/orangex/api/model/User.java
@@ -0,0 +1,90 @@
+package br.com.orangex.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+
+    @Indexed(unique = true)
+    private String username;
+
+    @Field("birth_date")
+    private LocalDate birthDate;
+
+    public User() {}
+
+    public User(String id, String name, String email, String username, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.birthDate = birthDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+}
---
+++ src/main/java/br/com/orangex/api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.orangex.api.repository;
+
+import br.com.orangex.api.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    //Finds user for authentication
+    UserDetails findByUsername(String username);
+
+}
